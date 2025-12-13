00f2cfe9545873ebdf152c6a5da7e7d57bf0d729
Vadym Kruchyna <kruchina.vadim@gmail.com>
2025-12-13 19:14:46 +0100

    quotes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
+fetch('https://your-energy.b.goit.study/api/quote')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    const quoteTextElement = document.getElementById('js-exercises-quote-text');
+    const quoteAuthorElement = document.getElementById(
+      'js-exercises-quote-author'
+    );
 
+    if (quoteTextElement) {
+      quoteTextElement.textContent = data.quote;
+    }
+
+    if (quoteAuthorElement) {
+      quoteAuthorElement.textContent = data.author;
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching quote:', error);
+  });
