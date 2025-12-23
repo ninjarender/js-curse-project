77e0be34a61f77280ed77e1a4830a2091c3f2502
Vadym Kruchyna <kruchina.vadim@gmail.com>
2025-12-23 01:48:45 +0100

    Add base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: command === 'serve' ? '/' : '/js-curse-project/',
     root: 'src',
     css: {
       preprocessorOptions: {
